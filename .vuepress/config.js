3256b1e22be1cde28742c320882c1cfc9e82ecff
ww <972743696@qq.com>
2021-05-01 20:03:13 +0800

    2021.5.1第三次提交

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "Plain-ww",
     base: '/Wblog/', //设置站点根路径 
     description: "开始学习",
-    dest: "dist",
+    dest: ".vuepress/dist",
     head: [
         ['link', { rel: "icon", href: "/favicon.ico" }],
         ['meta', { name: "viewport", content: "width=device-width,initial-scale=1,user-scalable=no" }]
